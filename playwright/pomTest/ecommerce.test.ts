f621fd8462170309f621b2cead6c06fa9cc6aaa8
amol-a-kale <61923973+amol-a-kale@users.noreply.github.com>
2024-12-28 12:16:28 +0530

    Update ecommerce.test.ts

--- playwright/pomTest/ecommerce.test.ts
+++ playwright/pomTest/ecommerce.test.ts
@@ -1,8 +1,11 @@
 import { expect } from "@playwright/test";
 import { test } from "../base/fixture";
 import * as data from "../test-data/commontest-data.json"
-
-
+// use for run in particular browser
+// test.use({
+//     browserName : "firefox"
+    
+// })
 test.describe("ecommerce test cases", async() =>{
     test("Register new user tc_001 ", async({page, baseURL, registerPage })=>{
         await page.goto(`${baseURL}route=account/register`);
